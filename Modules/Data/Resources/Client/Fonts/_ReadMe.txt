6c3f59b2768382e34ff022517cb337977c0b9f0f
Tawaruuk <tawaruuk@gmail.com>
2017-07-19 01:40:35 +0500

    711 > 789

---
+++ Modules/Data/Resources/Client/Fonts/_ReadMe.txt
@@ -0,0 +1,14 @@
+Шрифты, необходмые для маппера:
+  Mapper.fnt
+  OldDefault.fofnt
+  Numbers.fofnt
+  BigNumbers.fofnt
+  Special.fofnt
+  Default.fofnt
+  Thin.fofnt
+  Fat.fofnt
+  Big.fofnt
+
+Шрифты, необходмые для клиента:
+  Default.fofnt
+  Big2.fnt
--- Run_Client.cmd
+++ Run_Client.cmd
@@ -1,2 +1,4 @@
+MD Client\Data
 COPY Extension.link Client\Data\Extension.link
-START "CL" "SDK\Binaries\Client\FOnline64.exe" -WorkDir %~dp0Client -AddConfig %~dp0FOnline.cfg
+
+START "CL" "SDK\Binaries\Client\Windows\FOnline64.exe" -WorkDir %~dp0Client -AddConfig %~dp0FOnline.cfg
